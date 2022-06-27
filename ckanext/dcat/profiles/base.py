ce41cf6964be8a411972bf7378a35579835e209e
Eric Soroos <eric@derilinx.com>
2022-06-27 16:52:05 +0100

    Force datetimes to be to the second, not the microsecond
    the MQA _MIGHT_ be sensitive to the exact format, even though it's legal

--- ckanext/dcat/profiles/base.py
+++ ckanext/dcat/profiles/base.py
@@ -1288,8 +1288,13 @@ class RDFProfile(object):
                 default_datetime = datetime.datetime(1, 1, 1, 0, 0, 0)
                 _date = parse_date(value, default=default_datetime)
 
+                # EU MQA might not be able to parse microseconds
                 self.g.add(
-                    (subject, predicate, _type(_date.isoformat(), datatype=XSD.dateTime))
+                    (
+                        subject,
+                        predicate,
+                        _type(_date.isoformat(timespec="seconds"), datatype=XSD.dateTime),
+                    )
                 )
             except ValueError:
                 self.g.add((subject, predicate, _type(value)))
