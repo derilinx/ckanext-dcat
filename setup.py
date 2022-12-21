7de9f49633c499ad68bb6558631f1e15ed746331
Eric Soroos <eric@derilinx.com>
2022-12-21 15:18:47 +0100

    allow updates to ckantoolkit

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'rdflib<6.0',
         'rdflib-jsonld<=0.5.0',
         'geomet>=0.2.0',
-        'ckantoolkit==0.0.3',
+        'ckantoolkit>=0.0.3',
         'future>=0.18.2',
         'six',
     ],
