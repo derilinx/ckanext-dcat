6ae4a297e6b3c801f17d99bb233abb7a6532af20
Ben Soroos <bensoroos@derilinx.com>
2024-06-10 16:17:06 +0100

    Handle translated notes fields as descriptions
    As with previous commits, this appears to be a base dcat-ap issue,
    but is validated in the BReg SHACL shapes (that a description exists, that is)

--- ckanext/dcat/profiles.py
+++ ckanext/dcat/profiles.py
@@ -1039,16 +1039,22 @@ class EuropeanDCATAPProfile(RDFProfile):
         dataset_dict['extras'] = []
         dataset_dict['resources'] = []
 
+
         # Basic fields
         for key, predicate in (
                 ('title', DCT.title),
-                ('notes', DCT.description),
                 ('url', DCAT.landingPage),
                 ('version', OWL.versionInfo),
                 ):
             value = self._object_value(dataset_ref, predicate)
             if value:
                 dataset_dict[key] = value
+
+        for description in self.g.objects(dataset_ref, DCT.description):
+            if description.language:
+                dataset_dict.setdefault('notes_translated', {})[description.language] = description.value
+            else:
+                dataset_dict['notes'] = description.value
 
         if not dataset_dict.get('version'):
             # adms:version was supported on the first version of the DCAT-AP
@@ -1282,6 +1288,9 @@ class EuropeanDCATAPProfile(RDFProfile):
             ('provenance', DCT.provenance, None, Literal),
         ]
         self._add_triples_from_dict(dataset_dict, dataset_ref, items)
+
+        for (lang, notes) in dataset_dict.get('notes_translated', {}).items():
+            g.add((dataset_ref, DCT.description, Literal(notes, lang=lang)))
 
         # Tags
         for tag in dataset_dict.get('tags', []):
