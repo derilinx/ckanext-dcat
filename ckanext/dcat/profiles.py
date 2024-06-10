4bd51b2f5395fdb4115560e577757fa7bccf5f56
Ben Soroos <bensoroos@derilinx.com>
2024-06-10 16:21:18 +0100

    Add BReg DCAT AP profile
    At present, this only handles dataset fields, and some associated classes.
    In particular, no attempt is made at handling data service fields.

--- ckanext/dcat/profiles.py
+++ ckanext/dcat/profiles.py
@@ -37,6 +37,8 @@ GSP = Namespace('http://www.opengis.net/ont/geosparql#')
 OWL = Namespace('http://www.w3.org/2002/07/owl#')
 SPDX = Namespace('http://spdx.org/rdf/terms#')
 ELI= Namespace('http://data.europa.eu/eli/ontology#')
+CPSV = Namespace('http://purl.org/vocab/cpsv#')
+DQV = Namespace('http://www.w3.org/ns/dqv#')
 
 GEOJSON_IMT = 'https://www.iana.org/assignments/media-types/application/vnd.geo+json'
 
@@ -54,6 +56,8 @@ namespaces = {
     'gsp': GSP,
     'owl': OWL,
     'spdx': SPDX,
+    'cpsv': CPSV,
+    'dqv': DQV,
 }
 
 PREFIX_MAILTO = u'mailto:'
@@ -1826,6 +1830,47 @@ class EuropeanDCATAPHVD220Profile(EuropeanDCATAP2Profile):
         if 'http://data.europa.eu/eli/reg_impl/2023/138/oj' in \
            self._read_list_value(self._get_dataset_value(resource_dict, 'applicable_legislation')):
             return super().graph_from_resource(g, dataset_ref, resource_dict, resource_license_fallback, distribution)
+
+
+class EuropeanDCATAPBRegProfile(RDFProfile):
+    BREG_FIELDS = [
+        (DCT.identifier, 'identifiers', None),
+        (DCT.hasPart, 'has_part', DCAT.Dataset),
+        (DQV.hasQualityAnnotation, 'has_quality_annotation', DQV.QualityAnnotation),
+        (DQV.hasQualityMeasurement, 'has_quality_measurement', DQV.QualityMeasurement),
+        (DCT.isPartOf, 'is_part_of', DCAT.Dataset),
+        (DCT.isReplacedBy, 'is_replaced_by', DCAT.Dataset),
+        (DCT.isRequiredBy, 'is_required_by', DCAT.Dataset),
+        (DCT.references, 'references', RDFS.Resource),
+        (DCT.requires, 'requires', DCAT.Dataset),
+    ]
+    # TODO: Data Services, all the other BReg classes
+
+    def parse_dataset(self, dataset_dict, dataset_ref):
+        for (pred, key, cls) in self.BREG_FIELDS:
+            dataset_dict[key] = self._object_value_list(dataset_ref, pred)
+
+        dataset_dict['follows_rules'] = [
+            {
+                'identifier': str(rule),
+                'title': str(self.g.value(rule, DCT.title)),
+                'description': str(self.g.value(rule, DCT.description))
+            }
+            for rule in self.g.objects(dataset_ref, CPSV.follows)
+        ]
+
+
+    def graph_from_dataset(self, dataset_dict, dataset_ref):
+        for (pred, key, cls) in self.BREG_FIELDS:
+            self._add_triple_from_dict(dataset_dict, dataset_ref, pred, key, _type=URIRefOrLiteral, list_value=True, _class=cls)
+
+        for rule in dataset_dict.get('follows_rules', []):
+            ref = URIRef(rule['identifier'])
+            self.g.add((dataset_ref, CPSV.follows, ref))
+            self.g.add((ref, RDF.type, CPSV.Rule))
+            self.g.add((ref, DCT.identifier, Literal(str(ref))))
+            self.g.add((ref, DCT.title, Literal(rule['title'])))
+            self.g.add((ref, DCT.description, Literal(rule['description'])))
 
 
 class SchemaOrgProfile(RDFProfile):
