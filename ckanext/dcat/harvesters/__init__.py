03d71b0f6791b33bad6958a3e0b53f879eeb0528
Eric Soroos <eric@derilinx.com>
2024-06-24 17:07:44 +0000

    added esri harvester fix

--- ckanext/dcat/harvesters/__init__.py
+++ ckanext/dcat/harvesters/__init__.py
@@ -2,3 +2,5 @@ from ckanext.dcat.harvesters.rdf import DCATRDFHarvester
 
 from ckanext.dcat.harvesters.xml import DCATXMLHarvester
 from ckanext.dcat.harvesters._json import DCATJSONHarvester
+from ckanext.dcat.harvesters.esri_dcat_211 import EsriInspireDcat211Harvester
+
