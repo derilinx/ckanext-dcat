bcbbd8f2106f7ff3325f0a88631494da2f3860f4
amercader <amercadero@gmail.com>
2026-05-14 12:19:27 +0200

    Fix graph_from_resource calls in HVD profile

--- ckanext/dcat/profiles/euro_dcat_ap_hvd_22.py
+++ ckanext/dcat/profiles/euro_dcat_ap_hvd_22.py
@@ -2,9 +2,24 @@ from .euro_dcat_ap_2 import EuropeanDCATAP2Profile
 
 
 class EuropeanDCATAPHVD220Profile(EuropeanDCATAP2Profile):
-    ''' Read only catalog output for the HVD Profile, that only includes HVD resources '''
+    """Read only catalog output for the HVD Profile, that only includes HVD resources"""
 
-    def graph_from_resource(self, g, dataset_ref, resource_dict, resource_license_fallback, distribution=None):
-        if 'http://data.europa.eu/eli/reg_impl/2023/138/oj' in \
-           self._read_list_value(self._get_dataset_value(resource_dict, 'applicable_legislation')):
-            return super().graph_from_resource(g, dataset_ref, resource_dict, resource_license_fallback, distribution)
+    def graph_from_resource(
+        self,
+        dataset_dict,
+        dataset_ref,
+        resource_dict,
+        distribution_ref=None,
+        resource_license_fallback=None,
+    ):
+        legislation_value = "http://data.europa.eu/eli/reg_impl/2023/138/oj"
+        if legislation_value in self._read_list_value(
+            self._get_dataset_value(resource_dict, "applicable_legislation")
+        ):
+            return super().graph_from_resource(
+                dataset_dict,
+                dataset_ref,
+                resource_dict,
+                distribution_ref,
+                resource_license_fallback,
+            )
