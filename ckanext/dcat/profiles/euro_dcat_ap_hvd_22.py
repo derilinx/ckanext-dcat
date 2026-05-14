535241b55258c3efdc423c88fe13bc67c0868532
amercader <amercadero@gmail.com>
2026-05-14 14:47:53 +0200

    Fix HVD profile resource call, add tests
    The logic is:
    
    A dataset is parsed as HVD if
    
    * has the HVD legislation URI [1] as one of the values of the dataset
      `applicable_legislation` field, or
    * any of its resources has the HVD legislation URI as one of the values of the
      `applicable_legislation` resource field
    
    Within a dataset, a resource is parsed as HVD if
    
    * has the HVD legislation URI [1] as one of the values of the resource
      `applicable_legislation` field
    
    Added a profile-level test and a end user catalog endpoint one
    
    [1] http://data.europa.eu/eli/reg_impl/2023/138/oj

--- ckanext/dcat/profiles/euro_dcat_ap_hvd_22.py
+++ ckanext/dcat/profiles/euro_dcat_ap_hvd_22.py
@@ -1,8 +1,18 @@
 from .euro_dcat_ap_2 import EuropeanDCATAP2Profile
 
+LEGISLATION_HVD_URI = "http://data.europa.eu/eli/reg_impl/2023/138/oj"
+
 
 class EuropeanDCATAPHVD220Profile(EuropeanDCATAP2Profile):
     """Read only catalog output for the HVD Profile, that only includes HVD resources"""
+
+    def graph_from_dataset(self, dataset_dict, dataset_ref):
+
+        if any(
+            LEGISLATION_HVD_URI in r.get("applicable_legislation", [])
+            for r in dataset_dict["resources"]
+        ) or (LEGISLATION_HVD_URI in dataset_dict.get("applicable_legislation", [])):
+            return super().graph_from_dataset(dataset_dict, dataset_ref)
 
     def graph_from_resource(
         self,
@@ -12,10 +22,8 @@ class EuropeanDCATAPHVD220Profile(EuropeanDCATAP2Profile):
         distribution_ref=None,
         resource_license_fallback=None,
     ):
-        legislation_value = "http://data.europa.eu/eli/reg_impl/2023/138/oj"
-        if legislation_value in self._read_list_value(
-            self._get_dataset_value(resource_dict, "applicable_legislation")
-        ):
+
+        if LEGISLATION_HVD_URI in resource_dict.get("applicable_legislation", []):
             return super().graph_from_resource(
                 dataset_dict,
                 dataset_ref,
